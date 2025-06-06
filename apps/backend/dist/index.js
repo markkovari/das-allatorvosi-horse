5eeff4549b4ecb492bd6d8ee4386de9ec2e729e6
markkovari <kovarimarkofficial@gmail.com>
2025-06-06 10:31:39 +0200

    chore(backend): add start backend script
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>

---
+++ apps/backend/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = require("./app");
+app_1.app.listen(8000, () => console.log("Server running on port 8000"));
