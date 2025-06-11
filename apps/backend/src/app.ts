ddffdce89319d0a65cc189b9319981a1d826ceb2
sanyi <sandor.vass@tuta.io>
2025-06-11 14:55:33 +0200

    hello, it's me

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -4,21 +4,21 @@ import { z } from "zod";
 const app = express();
 
 const querySchema = z.object({
-	name: z.string(),
+  name: z.string(),
 });
 
 const counter: { [key: string]: number } = {};
 
 app.get("/count", (req, res) => {
-	const validation = querySchema.safeParse(req.query);
-	if (!validation.success) {
-		res.status(400).json({ error: validation.error });
-		return;
-	}
-	counter[validation.data.name] = (counter[validation.data.name] || 0) + 1;
+  const validation = querySchema.safeParse(req.query);
+  if (!validation.success) {
+    res.status(400).json({ error: validation.error });
+    return;
+  }
+  counter[validation.data.name] = (counter[validation.data.name] || 0) + 1;
 
-	res.status(201).json({ message: { name: counter[validation.data.name] } });
-	return;
+  res.status(200).json({ message: { name: counter[validation.data.name] } });
+  return;
 });
 
 export { app };
