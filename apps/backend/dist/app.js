ea8dfd741f51780d89f969725edd2049058abd5d
markkovari <kovarimarkofficial@gmail.com>
2025-06-18 14:02:42 +0200

    fix(): backend port and run command
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>

--- apps/backend/dist/app.js
+++ apps/backend/dist/app.js
@@ -5,5 +5,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
+const zod_1 = require("zod");
 const app = (0, express_1.default)();
 exports.app = app;
+const querySchema = zod_1.z.object({
+    name: zod_1.z.string(),
+});
+const counter = {};
+app.get("/count", (req, res) => {
+    const validation = querySchema.safeParse(req.query);
+    if (!validation.success) {
+        res.status(400).json({ error: validation.error });
+        return;
+    }
+    counter[validation.data.name] = (counter[validation.data.name] || 0) + 1;
+    res.status(201).json({ message: { name: counter[validation.data.name] } });
+    return;
+});
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,3 +1,3 @@
 import { app } from "./app";
 
-app.listen(8080, () => console.log("Server running on port 8000"));
+app.listen(8080, () => console.log("Server running on port 8080"));
