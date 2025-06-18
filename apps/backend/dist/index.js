3a6f34984bd96d204d7b924b0c330408f74a1a09
markkovari <kovarimarkofficial@gmail.com>
2025-06-18 14:08:30 +0200

    fix(appspec): add run and build command
    Signed-off-by: markkovari <kovarimarkofficial@gmail.com>

--- apps/backend/dist/index.js
+++ apps/backend/dist/index.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
-app_1.app.listen(8080, () => console.log("Server running on port 8000"));
+app_1.app.listen(8080, () => console.log("Server running on port 8080"));
